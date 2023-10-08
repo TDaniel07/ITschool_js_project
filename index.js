3cda967fa458d3dd133f6f4d5b6af1ceabf8868f
Tudor Daniel Andrei <tudordaniel56@gmail.com>
2023-10-08 21:02:22 +0300

    fixed default city

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const currentCityTag = document.querySelector(".city-name");
 let currentCity = localStorage.getItem("city");
 
 if(currentCity == undefined){
-    localStorage.setItem("city", "București")
+    localStorage.setItem("city", "București");
+    currentCity = localStorage.getItem("city");
 }
 
 currentCityTag.innerText = currentCity;
